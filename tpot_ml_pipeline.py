6ab3bbc13b948f1e25cd0687452bcdbc83c43d73
psyrgkan <psyrganis@gmail.com>
2022-12-08 22:01:46 +0100

    final draft before pres

--- tpot_ml_pipeline.py
+++ tpot_ml_pipeline.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import ElasticNetCV, SGDRegressor
+from sklearn.linear_model import ElasticNetCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
 from tpot.builtins import StackingEstimator
@@ -13,11 +13,10 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: -307.19222912682227
+# Average CV score on the training set was: -303.38237636728564
 exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=ElasticNetCV(l1_ratio=0.75, tol=0.01)),
-    StackingEstimator(estimator=SGDRegressor(alpha=0.001, eta0=0.01, fit_intercept=False, l1_ratio=0.25, learning_rate="invscaling", loss="squared_loss", penalty="elasticnet", power_t=100.0)),
-    RandomForestRegressor(bootstrap=True, max_features=0.4, min_samples_leaf=16, min_samples_split=14, n_estimators=100)
+    StackingEstimator(estimator=RandomForestRegressor(bootstrap=True, max_features=0.1, min_samples_leaf=19, min_samples_split=19, n_estimators=100)),
+    ElasticNetCV(l1_ratio=1.0, tol=0.1)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
--- tpot_ml_pipeline1.py
+++ tpot_ml_pipeline1.py
@@ -9,8 +9,8 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: -70.01234638952245
-exported_pipeline = XGBRegressor(learning_rate=0.1, max_depth=6, min_child_weight=14, n_estimators=100, n_jobs=1, objective="reg:squarederror", subsample=0.9500000000000001, verbosity=0)
+# Average CV score on the training set was: -69.69581244177242
+exported_pipeline = XGBRegressor(learning_rate=0.1, max_depth=8, min_child_weight=6, n_estimators=100, n_jobs=1, objective="reg:squarederror", subsample=0.6000000000000001, verbosity=0)
 # Fix random state in exported estimator
 if hasattr(exported_pipeline, 'random_state'):
     setattr(exported_pipeline, 'random_state', 42)
