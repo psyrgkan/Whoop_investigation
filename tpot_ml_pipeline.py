85af687162a9f438960cb7fbb1fae0f30740d205
psyrgkan <psyrganis@gmail.com>
2022-12-14 14:08:47 +0100

    finalized

--- tpot_ml_pipeline.py
+++ tpot_ml_pipeline.py
@@ -1,11 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import ElasticNetCV
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline, make_union
-from tpot.builtins import StackingEstimator
-from tpot.export_utils import set_param_recursive
+from xgboost import XGBRegressor
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -13,13 +9,11 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: -303.38237636728564
-exported_pipeline = make_pipeline(
-    StackingEstimator(estimator=RandomForestRegressor(bootstrap=True, max_features=0.1, min_samples_leaf=19, min_samples_split=19, n_estimators=100)),
-    ElasticNetCV(l1_ratio=1.0, tol=0.1)
-)
-# Fix random state for all the steps in exported pipeline
-set_param_recursive(exported_pipeline.steps, 'random_state', 42)
+# Average CV score on the training set was: -69.69581244177242
+exported_pipeline = XGBRegressor(learning_rate=0.1, max_depth=8, min_child_weight=6, n_estimators=100, n_jobs=1, objective="reg:squarederror", subsample=0.6000000000000001, verbosity=0)
+# Fix random state in exported estimator
+if hasattr(exported_pipeline, 'random_state'):
+    setattr(exported_pipeline, 'random_state', 42)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
